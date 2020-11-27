989b35f7d22c89c592d5365eb52cf01df010a2e7
김동현 <kimdonghyun@gimdonghyeon-ui-iMac.local>
2020-11-27 17:59:35 +0900

    todo-app 공부

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     ]);
 
 
+  const [ids, setIds] = useState(3); // 이미 0,1,2 가 존재하므로 3으로 설정
 
-  let id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
 
   function handleChange(e) {
     // this.setState({ fruit: 'orange' })와 같은 효과를 냅니다.
@@ -27,12 +27,14 @@ function App() {
     setInputs('');
     setTodos([
       ...todos,{
-      id: id,
+      id: ids,
       text: inputs,
       checked: false
       }
     ]);
-    id++;
+
+    setIds(ids + 1)
+    //id+=1;
   }
   function handleKeyPress (e) {
     // 눌려진 키가 Enter 면 handleCreate 호출
@@ -40,6 +42,29 @@ function App() {
       handleCreate();
     }
   }
+
+
+
+function handleToggle (id) {
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+    setTodos(nextTodos);
+  }
+
+  function handleRemove (id) {
+    const data = todos.filter(todo => todo.id !== id);
+        setTodos(data);
+  }
+
 
   return (
       <TodoListTemplate form={(
@@ -50,7 +75,7 @@ function App() {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
       </TodoListTemplate>
   );
 }
